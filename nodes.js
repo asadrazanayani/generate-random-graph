fd6333a2d2bcd4b4b78c4f5c91967465b5b8f2ea
asadrazanayani <asadrazanayani@gmail.com>
2022-11-16 11:49:25 -0500

    'new'

--- nodes.js
+++ nodes.js
@@ -1,126 +1,46 @@
 [
     {
         "id": "-1",
-        "label": "Chad"
+        "label": "Alexander"
     },
     {
         "id": "0",
-        "label": "Trent"
+        "label": "Terry"
     },
     {
         "id": "1",
-        "label": "Harold"
+        "label": "John"
     },
     {
         "id": "2",
-        "label": "Daniel"
+        "label": "Jonathon"
     },
     {
         "id": "3",
-        "label": "Sean"
-    },
-    {
-        "id": "4",
-        "label": "Terrance"
-    },
-    {
-        "id": "5",
-        "label": "Carroll"
-    },
-    {
-        "id": "6",
-        "label": "Clayton"
-    },
-    {
-        "id": "7",
-        "label": "David"
-    },
-    {
-        "id": "8",
-        "label": "Steven"
-    },
-    {
-        "id": "9",
-        "label": "Roger"
-    },
-    {
-        "id": "10",
-        "label": "Anthony"
-    },
-    {
-        "id": "11",
-        "label": "Kyle"
-    },
-    {
-        "id": "12",
-        "label": "Kenneth"
-    },
-    {
-        "id": "13",
-        "label": "David"
-    },
-    {
-        "id": "14",
-        "label": "Brett"
-    },
-    {
-        "id": "15",
         "label": "Wayne"
     },
     {
-        "id": "16",
-        "label": "Jesus"
-    },
-    {
-        "id": "17",
-        "label": "Mario"
-    },
-    {
-        "id": "18",
+        "id": "4",
         "label": "Robert"
     },
     {
-        "id": "19",
-        "label": "Clifford"
+        "id": "5",
+        "label": "Robert"
     },
     {
-        "id": "20",
-        "label": "Brent"
+        "id": "6",
+        "label": "Reginald"
     },
     {
-        "id": "21",
-        "label": "Dwayne"
+        "id": "7",
+        "label": "Robert"
     },
     {
-        "id": "22",
-        "label": "Glenn"
+        "id": "8",
+        "label": "Darren"
     },
     {
-        "id": "23",
-        "label": "Joseph"
-    },
-    {
-        "id": "24",
-        "label": "Kevin"
-    },
-    {
-        "id": "25",
-        "label": "Carlos"
-    },
-    {
-        "id": "26",
-        "label": "Justin"
-    },
-    {
-        "id": "27",
-        "label": "Walter"
-    },
-    {
-        "id": "28",
-        "label": "Jose"
-    },
-    {
-        "id": "29",
-        "label": "Jean"
+        "id": "9",
+        "label": "Jarvis"
     }
 ]
